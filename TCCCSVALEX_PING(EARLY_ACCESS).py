676ae98ed308d5344c0cd8067d8726af70ceb09c
Justin Powell <mastrranger@gmail.com>
2014-07-15 14:37:15 -0400

    The beginning
    Of the End

---
+++ TCCCSVALEX_PING(EARLY_ACCESS).py
@@ -0,0 +1,14 @@
+import socket
+
+s = socket.socket()
+host = socket.gethostname()
+port = 12345
+s.bind((host, port))
+
+s.listen(5)
+
+while True:
+    c, addr = s.accept()
+    print("Connection established from: ", addr)
+    c.send("Thank you for connecting")
+    c.close()
