da1a75765c1e44fa10651dcf831d1288f913e475
nickdsub <nickdsub@gmail.com>
2014-07-18 14:21:07 -0500

    Commented out different IP

--- Client.py
+++ Client.py
@@ -4,6 +4,7 @@ s = socket.socket()
 host = socket.gethostname()
 port = 12345
 
-s.connect(('192.168.56.1', port))
+s.connect(('99.108.229.133', port))
+#s.connect(('192.168.1.85', port))
 print(s.recv(1024))
 s.close
