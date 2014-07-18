54d016c3af8d4d5dfd6a2486a5df2d2062b1bc2e
nickdsub <nickdsub@gmail.com>
2014-07-18 14:54:34 -0500

    Input to server
    Client can now send messages to server

--- Client.py
+++ Client.py
@@ -4,7 +4,13 @@ s = socket.socket()
 host = socket.gethostname()
 port = 12345
 
-s.connect(('99.108.229.133', port))
-#s.connect(('192.168.1.85', port))
+#s.connect(('99.108.229.133', port))
+s.connect(('192.168.1.85', port))
 print(s.recv(1024))
-s.close
+
+st = "Message received"
+s.send(st.encode('utf-8'))
+
+while True:
+    st = input()
+    s.send(st.encode('utf-8'))
